12343e821377eaed2dd7568b931c1b2ca2780d21
Tyler Nguyen <tyler7nguyen@duck.com>
2026-06-09 18:00:06 -0400

    test: add canvas mock for cross-platform testing and update image generator tests

---
+++ __mocks__/canvas.js
@@ -0,0 +1,47 @@
+/**
+ * Manual mock for the 'canvas' npm package.
+ * Used for testing on Windows where the real canvas package
+ * cannot be installed without GTK/Cairo system libraries.
+ */
+
+class MockCanvas {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  getContext() {
+    return {
+      fillStyle: '',
+      strokeStyle: '',
+      lineWidth: 0,
+      font: '',
+      textAlign: '',
+      textBaseline: '',
+      fillRect: () => {},
+      beginPath: () => {},
+      roundRect: () => {},
+      fill: () => {},
+      stroke: () => {},
+      moveTo: () => {},
+      lineTo: () => {},
+      fillText: () => {},
+      drawImage: () => {}
+    };
+  }
+
+  toBuffer() {
+    return Promise.resolve(Buffer.from('fake-image'));
+  }
+}
+
+class MockImage {
+  constructor() {
+    this.src = null;
+  }
+}
+
+module.exports = {
+  createCanvas: (width, height) => new MockCanvas(width, height),
+  Image: MockImage
+};
